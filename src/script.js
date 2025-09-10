5693ccf5a280d6c2c0f25c1ac9749f523d2b225e
Sony VAIO <nsukadomingos@outlook.com>
2025-09-10 22:16:00 +0000

    Layout ("First Section and Secound Section Created")
    Fix problems in tailwind
    Layout Adjust

--- src/script.js
+++ src/script.js
@@ -1,12 +1,16 @@
 const menu_icon = document.getElementById('menu-icon');
+
 const menu_list = document.getElementById('menu-list');
-menu_icon.addEventListener("click", function(){
-    if(menu_list.classList.contains('hidden')){
+
+menu_icon.addEventListener("click", function () {
+    if (menu_list.classList.contains('hidden')) {
         menu_list.classList.add('h-auto');
         menu_list.classList.remove('hidden');
+        menu_list.classList.add('flex')
         menu_icon.innerHTML = '<span class="material-symbols-outlined">close</span>'
-    }else {
+    } else {
         menu_list.classList.add('hidden')
+        menu_list.classList.remove('flex')
         menu_icon.innerHTML = '<span class="material-symbols-outlined">menu</span>'
     }
 });
